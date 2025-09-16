0533a50c93fa84247df07e1c0bc13b23872b39ab
Aluno <aluno@acad.fanese.edu.br>
2025-09-16 19:52:53 -0300

    criacao

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("Olá mundo");
+
+var nome = "Ricardo Santana";
+const pi = 3.14;
+let idade = 28;
+//console.log(nome,"Idade: ",idade);
+
+nome = "Maria Maria"
+//console.log(nome,"Idade: ",idade);
+
+nome = "Call of Juarez"
+//console.log(nome,"Idade: ",idade);
+
+const soma = 1+5;
+let subtracao = 5-2; 
+let multi = 5*2;
+let divi = 10/2;
+let i = 0;
+//console.log(soma-subtracao,multi,divi)
+while (i<=25) {
+    if (i<=25) {
+        console.log(i);
+        i = i+1;
+    } else {
+        
+    }
+    
+}
+//pi = 123456;
